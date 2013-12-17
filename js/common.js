a2a4d5a4231e8d99c2862edbf8cbe84c024c9567
Splintex <godun.yevgen@gmail.com>
2013-12-17 17:42:01 +0200

    upd

--- js/common.js
+++ js/common.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-
+	if ($('.js-rate').length > 0) {
+		$('.js-rate').each(function(){
+			$(this).raty();
+		});
+		$('.js-rate.is-readonly').each(function(){
+			$(this).raty({
+				readOnly: true,
+				noRatedMsg: ""
+			});
+		});
+	}
+	
 });
